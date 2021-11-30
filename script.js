cc235492685b8df5fe4e034463feacaab11b19ce
antoine-moufflet <95289994+antoine-moufflet@users.noreply.github.com>
2021-11-30 17:07:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function foncEntre(){
+	document.querySelector("#maDiv").classList.remove("blanc")
+	document.querySelector("#maDiv").classList.add("rouge")
+}
+function foncQuitte(){
+	document.querySelector("#maDiv").classList.remove("rouge")
+	document.querySelector("#maDiv").classList.add("blanc")
+}
+alert("This is me ⚠️")
+function foncMaintenance(){
+	alert("Nous sommes désolé mais le site est en cour de maintenance ")
+}
